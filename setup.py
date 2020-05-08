f1fc1ddc9f2bcc745d243e659e794177c5e539ca
blasvicco@gmail.com <blasvicco@gmail.com>
2020-05-08 03:10:56 +0200

    Making it a package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''setup.py'''
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+from setuptools import setup, find_packages
+setup(
+  author='Blas',
+  name='gpu-image-corr',
+  version='0.1',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+  scripts=['src/main.py'],
+  entry_points={
+    'console_scripts': [
+      'gpu-image-corr=main:main',
+    ],
+  },
+)
